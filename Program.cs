ae1f84e2a9aa5fa155abd84c817952d8e56469d1
Ashgurung <66690289+Ashgurung@users.noreply.github.com>
2020-06-28 20:09:34 -0700

    Lab5_4

--- Program.cs
+++ Program.cs
@@ -1,43 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace lab5_2
+namespace lab5_4
 {
     class Program
     {
-
-        public enum WeekDays
+        public struct Mario
         {
+            public enum Size
+            {
+                small,
+                large
+            }
 
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
+            public int numOfLives;
+            public string currentLevel;
+            public Size sizeOfMario;
+            public bool canShootFireBalls;
+            public bool isJumping;
+            public int numOfCoins;
+
         }
 
         static void Main(string[] args)
         {
-            //Create a list to hold tasks
-            List<string> taskList = new List<string>();
-
-            //for each value in the Enum...let the user create a task.
-            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
-            {
-                Console.WriteLine("Enter a new task for " + day);
-                string task = day + " : " + Console.ReadLine();
-                taskList.Add(task);
-
-            }
+            Mario mario = new Mario();
+            mario.numOfLives = 3;
+            mario.currentLevel = "World 1-1";
+            mario.sizeOfMario = Mario.Size.small;
+            mario.canShootFireBalls = false;
+            mario.isJumping = false;
 
 
-            //print all of the tasks.
-            foreach (string task in taskList)
-            {
-                Console.WriteLine(task);
-            }
+            Console.WriteLine("Mario is running through the level and meets a Gooba. Set Mario to jumping so he can kill it");
+            mario.isJumping = true;
+
+            Console.WriteLine("Mario squashes the Gooba. Now he needs to land!");
+            mario.isJumping = false;
+
+            Console.WriteLine("Mario hits a block and finds a 1UP. Increase Mario's life by one.");
+            mario.numOfLives += 1;
+
+            Console.WriteLine("Mario finds a secret Fire Flower and can now shoot fireballs. Change mario!");
+            mario.canShootFireBalls = true;
+
+            Console.WriteLine("Mario finds another power up mushroom and gets big. Change Mario");
+            mario.sizeOfMario = Mario.Size.large;
+
+            Console.WriteLine("Mario finds a stash of gold coins. Give Mario 10 coins.");
+            mario.numOfCoins = 10;
+
+            Console.WriteLine("Mario finshes the level! Change Mario's current level to World 1-2");
+            mario.currentLevel = "World 1-2";
+
         }
     }
 }
