84a9294c1d05d8ffe8962268d7ebdaea6624ee15
Ashgurung <66690289+Ashgurung@users.noreply.github.com>
2020-06-28 19:13:50 -0700

    Lab4_5

--- Program.cs
+++ Program.cs
@@ -1,36 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Lab4_4
+namespace lab4_5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] blackList = {
-            "buy", "viagra", "XXX", "free money",
-                "lifetime offer", "send money", "bank account",
-                "nigeria", "online pharmacy", "h8te", "meet girls" };
-
-            string message = Console.ReadLine();
-            bool isSpam = false;
-            message = message.ToLower();
-            for (int i = 0; i < blackList.Length; i++)
-            {
-                if (message.Contains(blackList[i]))
-                {
-                    isSpam = true;
-                }
-            }
+            //Create the list of scores.
+            List<int> scores = new List<int>();
+            scores.Add(1200);
+            scores.Add(4400);
+            scores.Add(3700);
+            scores.Add(8900);
+            scores.Add(7200);
 
 
-            if (isSpam == true)
+            //sort the list in default order and print it.
+            scores.Sort();
+
+            foreach (int s in scores)
             {
-                Console.WriteLine("The message contained spam");
+                Console.WriteLine(s);
             }
-            else
+
+            //Reverse the list order and print it.
+            scores.Reverse();
+
+            foreach (int s in scores)
             {
-                Console.WriteLine("The Message is Ok.");
+                Console.WriteLine(s);
             }
+
+            //Add a new item to the list
+            scores.Add(2400);
+
+            //remove an item from the list
+            scores.Remove(4400);
+
         }
     }
 }
