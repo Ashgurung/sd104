625be9a5670ccaf29cbbc8b417db4425b9afeda0
Ashgurung <66690289+Ashgurung@users.noreply.github.com>
2020-06-28 20:04:02 -0700

    Lab5_2

--- Program.cs
+++ Program.cs
@@ -1,25 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Lab4_6
+namespace lab5_2
 {
     class Program
     {
+
+        public enum WeekDays
+        {
+
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
         static void Main(string[] args)
         {
-            string[] days = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-            string[] tasks = new string[7];
+            //Create a list to hold tasks
+            List<string> taskList = new List<string>();
 
-            for (int i = 0; i < days.Length; i++)
+            //for each value in the Enum...let the user create a task.
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
             {
-
-                Console.WriteLine("Type in a new Task for " + days[i]);
-                tasks[i] = days[i] + " : " + Console.ReadLine();
+                Console.WriteLine("Enter a new task for " + day);
+                string task = day + " : " + Console.ReadLine();
+                taskList.Add(task);
 
             }
 
-            for (int i = 0; i < days.Length; i++)
+
+            //print all of the tasks.
+            foreach (string task in taskList)
             {
-                Console.WriteLine(tasks[i]);
+                Console.WriteLine(task);
             }
         }
     }
