d6bb723ab578ba248e18f6848c0e82b19bcb89c2
Ashgurung <66690289+Ashgurung@users.noreply.github.com>
2020-07-14 13:21:54 -0700

    Lab_10_4

--- Program.cs
+++ Program.cs
@@ -1,77 +1,28 @@
 ﻿using System;
-using System.IO;
-using System.Text;
-namespace ConsoleAppLab9_4
+
+namespace _10_4Lab
 {
-    class Header
-    {
-        public const string open = "<h1>";
-        public const string close = "</h1>";
-
-        public string CreateHeader(string text)
-        {
-            string header = String.Concat(open, text, close, "\n");
-            return header;
-        }
-    }
-
-    class UnorderedList
-    {
-        public const string open = "<ul>";
-        public const string close = "</ul>";
-
-        public string CreateListItem(string text)
-        {
-            string open = "<li>\n";
-            string close = "</li>\n";
-
-            string listItem = String.Concat(open, text, close, "\n");
-            return listItem;
-        }
-
-        public StringBuilder CreateList(string[] listItems)
-        {
-
-            StringBuilder sb = new StringBuilder();
-            sb.Append(open);
-            foreach (string item in listItems)
-            {
-                sb.Append(item);
-            }
-            sb.Append(close);
-
-            return sb;
-        }
-
-    }
-
-
-
-
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main()
         {
-            string filename = "C://weblogs//9_4Lab.html";
-            Header header = new Header();
-            UnorderedList list = new UnorderedList();
-            StringBuilder sb = new StringBuilder();
 
-            Console.WriteLine("Enter text for HTML header.");
-            string headerElement = header.CreateHeader(Console.ReadLine());
-            string[] listItems = new string[3];
+            Artist prince = new Artist("Prince");
+            prince.AddToPlaylist("Purple Rain");
+            prince.AddToPlaylist("Little Red Corvette");
+            prince.AddToPlaylist("Kiss");
+            prince.AddToPlaylist("When Doves Cry");
 
-            for (int i = 0; i < listItems.Length; i++)
-            {
-                Console.WriteLine("Add another item to the list.");
-                listItems[i] = list.CreateListItem(Console.ReadLine());
-            }
+            MediaPlayer player = new MediaPlayer();
+            player.PlayMedia(prince);
 
-            StringBuilder listElement = list.CreateList(listItems);
-            sb.Append(headerElement);
-            sb.Append(listElement.ToString());
+            AudioBook howto = new AudioBook("How to Win Friends & Influence People", "Dale Carnegie");
+            player.PlayMedia(howto);
 
-            File.AppendAllText(filename, sb.ToString());
+            MovieSoundTrack starWars = new MovieSoundTrack("Star Wars");
+            starWars.SelectTrack("Opening Credits Theme");
+            player.PlayMedia(starWars);
 
         }
     }
